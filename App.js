cbc392bcc2d49c043412c25fc421604c2ab830d4
shkfnly <ashoka.finley@gmail.com>
2017-07-22 11:49:54 +0300

    refactor implementation of credentials

--- App.js
+++ App.js
@@ -5,33 +5,11 @@ import { Navigation } from 'react-native-navigation'
 import { Provider } from 'react-redux'
 // Store
 import store from 'circles/lib/store/store'
-import Icon from 'react-native-vector-icons/Ionicons'
 
 import KeypadScreen from 'circles/lib/components/KeypadScreen'
 import LoginScreen from 'circles/lib/containers/LoginContainer'
+import ProfileScreen from 'circles/lib/containers/ProfileContainer'
 
-class ProfileScreen extends React.Component {
-  render () {
-    return (
-      <View style={{flex: 1, alignItems: 'center', borderWidth: 1}}>
-        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', width: 335, marginTop: 20, maxHeight: 40}}>
-          <TouchableHighlight onPress={() => this.props.navigator.pop({
-          })} style={{alignItems: 'center', justifyContent: 'center', backgroundColor: '#50E3C2', width: 40, height: 40, borderRadius: 8}}>
-            <Text style={{fontSize: 20}}>X</Text>
-          </TouchableHighlight>
-        </View>
-        <Icon
-          name='ios-contact'
-          style={{fontSize: 120}}
-        />
-        <View style={{flex: 1}}>
-          <Text style={{fontSize: 20, fontWeight: 'bold'}}>Ashoka</Text>
-          <Text>Balance: 100</Text>
-        </View>
-      </View>
-    )
-  }
-}
 class QRScreen extends React.Component {
   render () {
     return (
@@ -66,7 +44,7 @@ class SearchScreen extends React.Component {
 }
 Navigation.registerComponent('LoginScreen', () => LoginScreen, store, Provider)
 Navigation.registerComponent('KeypadScreen', () => KeypadScreen)
-Navigation.registerComponent('ProfileScreen', () => ProfileScreen)
+Navigation.registerComponent('ProfileScreen', () => ProfileScreen, store, Provider)
 Navigation.startSingleScreenApp({
   screen: {
     screen: 'LoginScreen'
